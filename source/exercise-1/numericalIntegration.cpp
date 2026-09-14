8a21b5cbd1a559e785b1a65a5ba65a4a24f39854
Ria <kristoffer.berg8@gmail.com>
2026-09-14 16:31:08 +0200

    Added files for exercise 1

---
+++ source/exercise-1/numericalIntegration.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main () {
+    std::cout << "This has not been implemented yet!";
+    return 0;
+}
--- source/placeHolder/placeHolder.cpp
+++
@@ -1,3 +0,0 @@
-#include "placeHolder.hpp"
-
-int uselessFunction(int a, int b) { return a + b; }
