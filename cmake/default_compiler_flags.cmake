9a4e2e8dc12f2d21f11337db5750cad4919169c9
KriaPia <kristoffer.berg8@gmail.com>
2026-09-20 16:05:13 +0200

    Added release preset

--- cmake/default_compiler_flags.cmake
+++ cmake/default_compiler_flags.cmake
@@ -3,7 +3,13 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         /W4 /w14640 /permissive-
     )
 elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-    add_compile_options(
-        -Wall -Wextra -Wpedantic
-    )
+    if (CMAKE_BUILD_TYPE MATCHES "Debug") 
+        add_compile_options(
+            -Wall -Wextra -Wpedantic
+        )
+    else()
+        add_compile_options(
+            -Wall -Wextra -Wpedantic -O3
+        )
+    endif()
 endif()
